59498b7298cc45aff143196509352462959ea0cc
Legend971 <21076@students.mrgs.school.nz>
2024-05-23 13:39:38 +1200

    making a window for my app

---
+++ app_settings.py
@@ -0,0 +1,21 @@
+from tkinter import*
+from app_settings import*
+
+def __innit__(self):
+    window = Tk()
+    w_width = 500
+    w_height = 500
+    
+    bg_color = "grey"
+
+    window.title("Calorie Intake")
+    window.geometry(str(w_width) + "x" + str(w_height))
+
+    main_frame = Frame(background="white", width=w_width, height=w_height)
+    main_frame.pack()
+
+    hello_label = Label(text="Hello, World!")
+    hello_label.place(x=300, y=300)
+
+
+    window.mainloop()
--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 from tkinter import *
 
 window = Tk()
-window.geometry("300x300")
+w_width = 500
+w_height = 500
+    
+bg_color = "grey"
 window.title("Calorie Intake")
+window.geometry(str(w_width) + "x" + str(w_height))
+
+main_frame = Frame(background="white", width=w_width, height=w_height)
+main_frame.pack()
+
+hello_label = Label(text="Hello, World!")
+hello_label.place(x=300, y=300)
+
 
 window.mainloop()
