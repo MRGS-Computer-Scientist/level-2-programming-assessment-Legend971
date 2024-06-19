8d4b6397a6f4ff60e34e3a7466056eb67c3041b9
Legend971 <21076@students.mrgs.school.nz>
2024-06-19 14:51:13 +1200

    created function for a window using innit, gave the window a name and set its size to 400x800 , setup different frames in the main window,  and added a method to show a specific frame while hiding the current frame.

--- app.py
+++ app.py
@@ -1,37 +1,16 @@
-from tkinter import *
-from app_settings import *
-from os import *
+import tkinter as tk
+from tkinter import Canvas, messagebox, Text
+from datetime import datetime
 
 class App():
+    def __init__(self, window):
+        # Initialize main window
+        self.window = window
+        self.window.title("Calorie Tracker")
+        self.window.geometry("400x800")
 
-    
-    def __init__(self):
-        self.window = Tk()
-        self.window.geometry(str(w_width) + "x" + str(w_height))
-        self.window.title(app_title)
+        # Initialize frames
+        self.init_frames()
 
-        self.top_frame = Frame(background='red', width=w_width, height=100)
-        self.top_frame.pack()
-
-        self.main_frame = Frame(background=bg_color, width=w_width, height=(w_height-200))
-        self.main_frame.pack()
-
-        self.bottom_frame = Frame(background='blue', width=w_width, height=100)
-        self.bottom_frame.pack(side='bottom')
-
-        self.home_button = Button(self.bottom_frame, text="Home", height=2, width=5, bg='green')
-        self.home_button.place(x=0,y=0)
-
-        self.exit_button = Button(self.bottom_frame, text="Exit", height=2, width=5, bg='green', command=exit)
-        self.exit_button.place(x=100,y=0)
-
-        self.dirname = path.dirname(__file__)
-        self.filename = path.join(self.dirname, 'images/')
-
-        print("The path is", self.filename)
-
-        self.window.mainloop()
-
-
-    def exit(self):
-        self.window.destroy()
+        # Show the cover page initially
+        self.show_frame(self.cover_frame)
